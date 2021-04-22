9847cea3ef66bf02575c64ce46550c46e9bbe444
dkolasani <kolasanidinesh>
2021-04-22 16:31:41 +0530

    second merge with conflict

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     
     // first commit in beta branch.
     //fixing bugs.
+    //creating conflict.
     
     override func viewDidLoad() {
         super.viewDidLoad()
